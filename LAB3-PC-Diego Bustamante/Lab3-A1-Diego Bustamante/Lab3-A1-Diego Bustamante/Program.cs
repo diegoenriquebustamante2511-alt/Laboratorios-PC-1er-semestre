35d3dc3859810334531d6b569fa489766928597c
diegoenriquebustamante2511-alt <diegoenrique.bustamante2511@gmail.com>
2026-02-05 14:52:37 -0600

    LAB3

---
+++ LAB3-PC-Diego Bustamante/Lab3-A1-Diego Bustamante/Lab3-A1-Diego Bustamante/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Hola, ingrese su nombre:");
+string nombre = Console.ReadLine();
+Console.WriteLine("¿Qué curso está tomando?");
+string curso = Console.ReadLine();
+Console.WriteLine("Hola " + nombre + "Bienvenido a " + curso);
+Console.WriteLine("Lo bueno que estás en sistemas, imagináte ser de psico ");
+Console.ReadKey();
